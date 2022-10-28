41aca20593d4ab5973663b488212b29924f762fb
Sabha Parameswaran <sabhap@amazon.com>
2022-10-28 13:03:19 -0700

    Handle additional shipments in loop

--- functions/shipment_address_grouper/shipment_grouper.py
+++ functions/shipment_address_grouper/shipment_grouper.py
@@ -6,23 +6,39 @@ tableName = os.getenv('SHIPMENT_HASH_TABLE')
 dynamodb = boto3.resource('dynamodb')
 
 def findAddressPartitions(addrHashCode):
+
+    records = []
+    moreRows = True
+    exclusiveStartKey = None
     table = dynamodb.Table(tableName)
 
-    exprValue = { }
-    filtrExpression='attribute_not_exists(sawbHash) '
+    while (moreRows):
+        exprValue = { }
+        filtrExpression='attribute_not_exists(sawbHash) '
 
-    if addrHashCode is not None:
-        exprValue[':addrHashCodeValue'] = addrHashCode
-        filtrExpression = filtrExpression + ' and addrHashCode = :addrHashCodeValue'
+        if addrHashCode is not None:
+            exprValue[':addrHashCodeValue'] = addrHashCode
+            filtrExpression = filtrExpression + ' and addrHashCode = :addrHashCodeValue'
 
-    args = { 'FilterExpression': filtrExpression, 'ProjectionExpression': 'addrHashCode,addrDateHash' }
-    if exprValue:
-        args['ExpressionAttributeValues'] = exprValue
+        args = { 'FilterExpression': filtrExpression, 'ProjectionExpression': 'addrHashCode,addrDateHash' }
 
-    #print('Incoming addrHashCode: {} and expValue: {} and filtrExpression: {}'.format(addrHashCode, exprValue, filtrExpression))
-    hashIdsResponse = table.scan(**args)
-    print(hashIdsResponse)
-    return hashIdsResponse['Items']
+        if exprValue:
+            args['ExpressionAttributeValues'] = exprValue
+
+        if exclusiveStartKey:
+            args['ExclusiveStartKey'] = exclusiveStartKey
+
+        hashIdsResponse = table.scan(**args)
+        print(hashIdsResponse)
+        for row in hashIdsResponse['Items']:
+            records.append(row)
+
+        exclusiveStartKey=hashIdsResponse.get('LastEvaluatedKey')
+        if exclusiveStartKey is not None:
+                print('More paginated rows left in {}!!'.format(tableName))
+        else:
+            moreRows = False
+    return records
 
 
 def createUniqueList(givenList):
@@ -36,8 +52,9 @@ def findAddressGroups(event, context):
     # Find all available unique addresses by their hash, dont search for specific one.
     # findAddressByDatePartitions would try to pull with a given addrHashCode
     #print('Incoming payload for findAddrByHash: ', event)
-    addrHashCode=None #event.get('addrHashCode')
+    addrHashCode=None
     addressPartitions = findAddressPartitions(addrHashCode)
+    print('Address partitions:', addressPartitions)
     addressList = [d['addrHashCode'] for d in addressPartitions]
 
     return createUniqueList(addressList)
